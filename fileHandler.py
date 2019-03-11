174aede44e49020087ed6ece7dfbee5d4824bdbf
cey0030 <48426058+cey0030@users.noreply.github.com>
2019-03-11 17:52:22 +1300

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -1,23 +1,16 @@
 import re
 
 file = open('classDiagram.txt', 'r').readlines()
+output = open("outputClassDiagram.txt", "w")
 print(file)
 
 def fileReader():
     for listItem in file:
         if "class" in listItem:
-            replaceList = [" ","class","{","\n"]
-            for x in replaceList:
-                listItem = listItem.replace(x, "")
-
-            print(listItem + ".py")
-
-
-
-
-
-
-
-
+            output.write(listItem + "   def __init__(")
+        if ":" in listItem:
+            output.write(listItem.strip()) #Upon finding the first whitespace, remove everything after in listItem
 
 file = fileReader()
+
+
