45389c69f92be35f27471e2fb1fec340890d2c52
cey0030 <48426058+cey0030@users.noreply.github.com>
2019-03-12 22:40:06 +1300

    Update fileHandler.py

--- fileHandler.py
+++ fileHandler.py
@@ -2,14 +2,32 @@ import re
 
 file = open('classDiagram.txt', 'r').readlines()
 output = open("outputClassDiagram.txt", "w")
-print(file)
 
+def classHandler():
+    classList = [[]]
+    for i, m in enumerate(file[1:-1]):
+        if m == "\n":
+            if i != len(file[1:-1]) - 1:
+                classList.append([])
+        else:
+            classList[-1].append(m)
+    return classList
 def fileReader():
-    for listItem in file:
-        if "class" in listItem:
-            output.write(listItem + "   def __init__(")
-        if ":" in listItem:
-            output.write(listItem.split(' ')[3])
-            print(listItem.split(' ')[3])
+    #for classItem in classHandler(): #for each class array in the array of classes,
+        classList = []
+        for listItem in classItem: #for each list item in each class array,
+            if "class" in listItem:
+                output.write(listItem + "   def __init__(") #append each class name to the array of classes
+                print(classList)
 
-file = fileReader()
+
+
+fileReader()
+
+#for listItem in rawcode:
+   #if "class" in listItem:
+       #classList.append(listItem.split(' ')[0:2])
+   # if ":" in listItem:
+       # output.write()
+        # print(listItem.split(' ')[3] + ',')
+
