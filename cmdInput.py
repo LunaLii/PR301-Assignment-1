212f87f2b16842a5d4a3c1325b0f7c4669597901
cey0030 <48426058+cey0030@users.noreply.github.com>
2019-03-19 16:31:13 +1300

    Update cmdInput.py

--- cmdInput.py
+++ cmdInput.py
@@ -1,35 +1,57 @@
 from fileHandler import printClass
 from cmd import Cmd
+import os
 
 
 class fileInput(Cmd):
     def __init__(self):
         Cmd.__init__(self)
         self.prompt = ">>> "
+        self.the_file = "classDiagram.txt"
 
-    def do_input(self, the_string):
+    def do_file(self, the_file):
+        """
+        Syntax: file the_file
+        Sets the file to write to
+        :param the_file: filename you want to write to (no quotations)
+        :return: None
+        """
+
+        if the_file:
+            self.the_file = the_file
+        print('You are now writing to ' + self.the_file)
+
+    def do_input(self, the_input):
         """
         Syntax: input [option] the_string
         Writes plantUML to a file
-        :param [option] : % (represents leading four spaces)
-        :param the_string : a string representing one line of a plantUML file
+        :param [option]: % (represents leading four spaces)
+        :param the_input: a string representing one line of a plantUML file
+        For an empty space, just type input then press enter
         :return: None
         """
-        with open('classDiagram.txt', 'a') as output:
-            if the_string[0:1] == '%':
-                output.write('   ' + the_string[1:] + '\n')
+        with open(self.the_file, 'a') as output:
+            if the_input[0:1] == '%':
+                output.write('   ' + the_input[1:] + '\n')
             else:
-                output.write(the_string + '\n')
+                output.write(the_input + '\n')
 
-    def do_printClass(self, line):
-        this = printClass('classDiagram.txt')
-        this.outputClasses()
+    def do_writeCode(self, line):
+        """
+        Syntax: writeCode
+        Creates new files and writes class to each
+        :return: None
+        """
+        if os.stat(self.the_file).st_size != 0:
+            printClass(self.the_file).outputClasses()
+        else:
+            print("Error! Writing to empty file!")
 
     def do_quit(self, line):
-        print("Quitting ......")
+        print("exiting ......")
         return True
 
-    def help_quit(self):
+    def help_exit(self):
         print("\n".join(['Quit from python CMD', ':return: True']))
 
     do_q = do_quit
